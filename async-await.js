93153dc29d066f29c99fb8cda1f1b4b01db3b3d9
yashraj8703 <rajyash8703@gmail.com>
2025-08-17 23:12:43 +0530

    added practice codes

--- async-await.js
+++ async-await.js
@@ -1,5 +1,7 @@
 //* .then -> await
 
+const { log } = require("node:console");
+
 function myPromise() {
   return new Promise(function (resolve) {
     setTimeout(function () {
@@ -17,7 +19,7 @@ function main() {
   });
 }
 
-main();
+// main();
 
 //* using async-await
 
@@ -30,13 +32,64 @@ function myPromise() {
 }
 
 async function main() {
-  const val = await myPromise(); // thread is waiting here
+  const val = await myPromise(); // like - thread is waiting here
   console.log(myPromise());
   console.log(val);
   console.log("Hi There1");
-
 }
 
-main();
+// main();
 
 // console.log("Hello World");
+
+//more on this
+
+// sync code
+//async code - first process this then only go to 3 rd block
+// sync cpde
+
+// if we want top behave this like sync we can use async await
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then(res => res.json())
+//   .then(data => console.log("From fetch:", data))
+//   .catch(err => console.error(err));
+// Example with your own Promise
+// js
+// Copy code
+// function wait(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(`Waited ${ms}ms`), ms);
+//   });
+// }
+
+// wait(2000)
+//   .then(msg => console.log(msg))
+//   .catch(err => console.error(err));
+
+
+//! async function always returns promise
+
+async function getData() {
+  setTimeout(() => console.log("Hi"), 2000);
+}
+
+// getData();
+// console.log(getData);
+
+//? await - why?
+
+console.log("FIRST");
+
+
+async function getAPI() {
+  let res=await fetch("https://jsonplaceholder.typicode.com/todos/1") //promise unfulfilled
+  let data=await res.json //json parsing is asyn
+  console.log(data);
+  
+}
+
+
+getAPI()
+console.log("second");
--- promise.js
+++ promise.js
@@ -1,6 +1,7 @@
 // // promise - creating our own async function
 
 const { log } = require("node:console");
+const { resolve } = require("node:path");
 
 // //! when using promise
 // //! no call backs - introduced to get rid of promise - call back hell
@@ -34,24 +35,79 @@ const { log } = require("node:console");
 
 // a.then(onDone);
 
-// //
-
 
 
 //! Example where promise is immediately resolved
 
 var d = new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve("foo");
-    }, 1000);
-//   resolve('foo');
+  setTimeout(function () {
+    resolve("foo");
+  }, 1000);
+  //   resolve('foo');
 });
 
 function callBack() {
   console.log(d);
 }
 
-console.log(d);
-d.then(callBack);
+// console.log(d);
+// d.then(callBack);
 
 //* .then() - runs when promise is resolved
+
+//more - practice
+
+// let firstPromise=new Promise((res,rej)=>{
+//   return res("Hi")
+// })
+
+// console.log(firstPromise)
+
+// let failedPromise = new Promise((res, rej) => {
+//   let success=true
+//   if(success) rej("Something went wrong");
+//   else res("fulfilled")
+// });
+// console.log(failedPromise);
+
+// failedPromise
+//   .then((data)=>{
+//     console.log(data);
+//   })
+//   .catch((err)=>{
+//     console.log(err);
+//   })
+
+let myPromise = new Promise((res, rej) => {
+  let success;
+  setTimeout(() => {
+    success = true;
+    if (success) res("resolved/fufilled");
+    else rej("Not fulfilled");
+  }, 3000);
+});
+
+// console.log(myPromise);
+// myPromise
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+
+const myAPIcall = "{\"userId\":1,\"id\":1,\"title\":\"delectus aut autem\",\"completed\":false}";
+let myPromo=new Promise((res,rej)=>{
+  setTimeout(() => {
+    res(myAPIcall)
+  }, 3000);
+})
+
+myPromo
+  .then((res)=> JSON.parse(res))
+  .then((res)=>{
+    console.log(res);
+  })
+  .catch((err)=>{console.log(err);
+  })
